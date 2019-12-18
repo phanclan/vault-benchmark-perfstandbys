b0180e8269ae856e27146ca6e6fa0c3f6f4868cf
Peter Phan <pphan@hashicorp.com>
2019-12-17 19:15:46 -0800

    fix ldap

--- scripts/00_fast_setup.sh
+++ scripts/00_fast_setup.sh
@@ -196,47 +196,58 @@ vault write -f ${TRANSIT_PATH}/keys/hr
 
 
 
-# tput clear
-# cyan "#-------------------------------------------------------------------------------
-# # Enable and Configure the LDAP Auth Method
-# #-------------------------------------------------------------------------------\n"
+tput clear
+cyan "#-------------------------------------------------------------------------------
+# Enable and Configure the LDAP Auth Method
+#-------------------------------------------------------------------------------\n"
 
-# green "Enable ldap auth method for two paths."
-# set +e
-# vault auth enable -path=ldap-um ldap
-# vault auth enable -path=ldap-mo ldap
-# set -e
+green "Enable ldap auth method for two paths."
+set +e
+vault auth enable -path=ldap-um ldap
+vault auth enable -path=ldap-mo ldap
+set -e
 
-# green "Configure connection details for your LDAP server, 
-# information on how to authenticate users, 
-# and instructions on how to query for group membership. 
-# The configuration options are categorized and detailed below."
+green "Configure connection details for your LDAP server, 
+information on how to authenticate users, 
+and instructions on how to query for group membership. 
+The configuration options are categorized and detailed below."
 
-# green "Configure Unique Member group lookups"
-# # Using group of unique names lookups
-# vault write auth/ldap-um/config \
-#     url="${LDAP_URL}" \
-#     binddn="${BIND_DN}" \
-#     bindpass="${BIND_PW}" \
-#     userdn="${USER_DN}" \
-#     userattr="${USER_ATTR}" \
-#     groupdn="${GROUP_DN}" \
-#     groupfilter="${UM_GROUP_FILTER}" \
-#     groupattr="${UM_GROUP_ATTR}" \
-#     insecure_tls=true
+green "Configure Unique Member group lookups"
+# Using group of unique names lookups
+echo vault write auth/ldap-um/config \
+    url="${LDAP_URL}" \
+    binddn="${BIND_DN}" \
+    bindpass="${BIND_PW}" \
+    userdn="${USER_DN}" \
+    userattr="${USER_ATTR}" \
+    groupdn="${GROUP_DN}" \
+    groupfilter="${UM_GROUP_FILTER}" \
+    groupattr="${UM_GROUP_ATTR}" \
+    insecure_tls=true
+
+vault write auth/ldap-um/config \
+    url="${LDAP_URL}" \
+    binddn="${BIND_DN}" \
+    bindpass="${BIND_PW}" \
+    userdn="${USER_DN}" \
+    userattr="${USER_ATTR}" \
+    groupdn="${GROUP_DN}" \
+    groupfilter="${UM_GROUP_FILTER}" \
+    groupattr="${UM_GROUP_ATTR}" \
+    insecure_tls=true
 
 
-# green "Configure MemberOf group lookups"
-# vault write auth/ldap-mo/config \
-#     url="${LDAP_URL}" \
-#     binddn="${BIND_DN}" \
-#     bindpass="${BIND_PW}" \
-#     userdn="${USER_DN}" \
-#     userattr="${USER_ATTR}" \
-#     groupdn="${USER_DN}" \
-#     groupfilter="${MO_GROUP_FILTER}" \
-#     groupattr="${MO_GROUP_ATTR}" \
-#     insecure_tls=true
+green "Configure MemberOf group lookups"
+vault write auth/ldap-mo/config \
+    url="${LDAP_URL}" \
+    binddn="${BIND_DN}" \
+    bindpass="${BIND_PW}" \
+    userdn="${USER_DN}" \
+    userattr="${USER_ATTR}" \
+    groupdn="${USER_DN}" \
+    groupfilter="${MO_GROUP_FILTER}" \
+    groupattr="${MO_GROUP_ATTR}" \
+    insecure_tls=true
 
 # #-------------------------------------------------------------------------------
 
